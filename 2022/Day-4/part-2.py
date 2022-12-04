d194b38590241671503177e61cbbbcdc264bf45e
Devansh Sharma <devansh.sharma@gmail.com>
2022-12-04 23:47:52 +0530

    Add 2022 Day-4 challenge

---
+++ 2022/Day-4/part-2.py
@@ -0,0 +1,24 @@
+print(
+    len(
+        list(
+            filter(
+                lambda x: (
+                    (x[0][0] <= x[1][0] and x[0][1] >= x[1][0])
+                    or (x[0][0] <= x[1][1] and x[0][1] >= x[1][1])
+                    or (x[0][0] >= x[1][0] and x[0][0] <= x[1][1])
+                    or (x[0][1] >= x[1][0] and x[0][1] <= x[1][1])
+                ),
+                [
+                    (
+                        [int(i) for i in x[0].split("-")],
+                        [int(i) for i in x[1].split("-")],
+                    )
+                    for x in [
+                        line.strip("\n").split(",")
+                        for line in open("input").readlines()
+                    ]
+                ],
+            )
+        )
+    )
+)
